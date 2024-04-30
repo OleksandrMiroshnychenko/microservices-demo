d1e23f38d524a60494571e549ca2a55a093876cb
Oleksandr Miroshnychenko <omiroshnychenko@c11839.lan>
2024-05-01 00:09:24 +0200

    Setup RegistryService

--- catalog-service/src/main/kotlin/com/miroshnychenko/catalog/CatalogServiceApplication.kt
+++ catalog-service/src/main/kotlin/com/miroshnychenko/catalog/CatalogServiceApplication.kt
@@ -2,7 +2,9 @@ package com.miroshnychenko.catalog
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
+@EnableEurekaClient
 @SpringBootApplication
 class CatalogServiceApplication
 
---
+++ registry-service/src/test/kotlin/com/miroshnychenko/registry/RegistryServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.miroshnychenko.registry
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
